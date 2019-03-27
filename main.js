6d96c36adfbf81baf81fbe5776dd8f1935e944ef
bjgribb <bjgribb@gmail.com>
2019-03-27 14:34:41 -0400

    can fetch based on input search text, need to research how to iterate thru and pull out values to display on webpage.

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-var search = document.querySelector('.search input')
-var searchButton = document.querySelector('.search button')
-var apiUrl = 'https://itunes-api-proxy.glitch.me/search?term='
+const search = document.querySelector('.search input')
+const searchButton = document.querySelector('.search button')
+const apiUrl = 'https://itunes-api-proxy.glitch.me/search?term='
 
 
 // This is working to format the url based on user text input so we can drop in fetch method
 function searchApi () {
     searchButton.addEventListener('click', function() {
         console.log(apiUrl.concat(search.value.trim().split(' ').join('+')))
+        let url = (apiUrl.concat(search.value.trim().split(' ').join('+')))
+        console.log(fetch(url))
     })
 }
 
