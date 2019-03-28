6eb46be4fcafa4ae2e3feeac033c59a341d644b4
bjgribb <bjgribb@gmail.com>
2019-03-28 11:44:36 -0400

    updated with notes from class

--- main.js
+++ main.js
@@ -3,13 +3,25 @@ const searchButton = document.querySelector('.search button')
 const apiUrl = 'https://itunes-api-proxy.glitch.me/search?term='
 
 
-// This is working to format the url based on user text input so we can drop in fetch method
-function searchApi () {
+// This is working to format the url based on user text and fetch and return song data
+function userSearch () {
     searchButton.addEventListener('click', function() {
-        console.log(apiUrl.concat(search.value.trim().split(' ').join('+')))
-        let url = (apiUrl.concat(search.value.trim().split(' ').join('+')))
-        console.log(fetch(url))
+        var url = (apiUrl.concat(encodeURIComponent(search.value)))
+        fetch(url).then(promise => promise.json())
+        .then(data => { for (let song of Object.values((data))[1])
+            console.log(song.trackName)
+            })
     })
 }
 
-searchApi()
+
+userSearch()
+
+// NOTES:
+
+// Used to figure out promise structure
+// var blackSabbath = fetch('https://itunes-api-proxy.glitch.me/search?term=black+sabbath')
+//     .then(promise => promise.json()) //.then(data => Object.data(data)))
+// console.log(blackSabbath.then(data => Object.values(data)[1][45].trackName))
+
+// clearing element clear innerHTML
